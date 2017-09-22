e4ea2980a24f332d2c80287a4273505e35b5ffa3
Harvey Siy <hsiy@unomaha.edu>
2017-09-22 17:35:48 -0500

    refactored plotlyOut initialization

--- rpi/plotlyOut.py
+++ rpi/plotlyOut.py
@@ -3,71 +3,58 @@
 import plotly.plotly as py
 from plotly.graph_objs import Scatter, Layout, Figure
 
-username = 'hsiy'
-api_key = 'y1YXzt7TIsc7RRY2TMAb'
-stream_token = ['2xc6yk9l09', 'euxrawpca5', 'ekbgwm6lcd']
-py.sign_in(username, api_key)
-trace1 = Scatter(
-	name='X',
-	x=[],
-	y=[],
-	stream=dict(
-		token=stream_token[0],
-		maxpoints=200
+trace = []
+stream = []
+numdata = 0
+datalabels = []
+
+def initOutput(labels):
+	global trace, stream, numdata, datalabels
+
+	username = 'hsiy'
+	api_key = 'y1YXzt7TIsc7RRY2TMAb'
+	stream_token = ['2xc6yk9l09', 'euxrawpca5', 'ekbgwm6lcd']
+	py.sign_in(username, api_key)
+
+	linecolor = ['rgb(0,0,255)', 'rgb(255,0,0)', 'rgb(255,255,0)']
+	datalabels = labels
+	numdata = min(3, len(labels))
+	for i in range(numdata):
+		trace.append(Scatter(
+			name=labels[i],
+			x=[],
+			y=[],
+			stream=dict(
+				token=stream_token[i],
+				maxpoints=200
+			), 
+			marker=dict(
+				color=linecolor[i]
+			)
+		)) 
+
+	layout = Layout(
+		title='Raspberry Pi Streaming Sensor Data'
 	)
-)
-trace2 = Scatter(
-	name='Y',
-	x=[],
-	y=[],
-	stream=dict(
-		token=stream_token[1],
-		maxpoints=200
-	), 
-	marker=dict(
-		color='rgb(255,0,0)'
-	)
-)
-trace3 = Scatter(
-	name='Z',
-	x=[],
-	y=[],
-	stream=dict(
-		token=stream_token[2],
-		maxpoints=200
-	), 
-	marker=dict(
-		color='rgb(255,255,0)'
-	)
-)
+	fig = Figure(data=trace, layout=layout)
+	print py.plot(fig, filename='Raspberry Pi Streaming Example Values')
 
 
-layout = Layout(
-	title='Raspberry Pi Streaming Sensor Data'
-)
-
-fig = Figure(data=[trace1,trace2,trace3], layout=layout)
-
-print py.plot(fig, filename='Raspberry Pi Streaming Example Values')
+	for i in range(numdata):
+		stream.append(py.Stream(stream_token[i]))
+		stream[i].open()
 
 
-stream1 = py.Stream(stream_token[0])
-stream1.open()
-stream2 = py.Stream(stream_token[1])
-stream2.open()
-stream3 = py.Stream(stream_token[2])
-stream3.open()
 t = 0
 
-def displayValues(data, labels):
-	global t
-	if len(data) != len(labels):
+def displayValues(data):
+	global t, stream, numdata, datalabels
+	if len(data) != len(datalabels):
 		print 'Length of data and labels do not match'
 		return
 
-	stream1.write({'x': t, 'y': data[0]})
-	stream2.write({'x': t, 'y': data[1]})
-	stream3.write({'x': t, 'y': data[2]})
+	for i in range(numdata):
+		stream[i].write({'x': t, 'y': data[i]})
 	t += 1
 
 
